646a6b31f0e3ad14ccd051968faad91360b8128d
Maria108 <maria.abash@gmail.com>
2018-06-02 19:33:01 -0400

    Fixed dynamoDB

--- src/handlers/submit.js
+++ src/handlers/submit.js
@@ -1,11 +1,12 @@
 const querystring = require('querystring');
 const AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
 
 export const submit = (event, context, callback) => {
   const requestBody = querystring.parse(event.body);
   const { text, name } = requestBody;
 
-  dbCreateItem('123', text, name);
+  dbCreateItem(text, name);
 
   const response = {
     statusCode: 200,
@@ -18,22 +19,20 @@ export const submit = (event, context, callback) => {
   callback(null, response);
 };
 
-function dbCreateItem(id, text, name) {
-  const docClient = new AWS.DynamoDB.DocumentClient();
-
-  const table = 'voiceMailChangerTable';
+function dbCreateItem(text, name) {
+  const ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
 
   const params = {
-    TableName: table,
+    TableName: 'voiceMailChangerTable',
     Item: {
-      id,
-      text,
-      name,
+      id: { S: uuidv4() },
+      text: { S: text },
+      name: { S: name },
     },
   };
 
   console.log('Adding a new item...');
-  docClient.put(params, (err, data) => {
+  ddb.putItem(params, (err, data) => {
     if (err) {
       console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
     } else {
