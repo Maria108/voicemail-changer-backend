86bf2354c27c6f5b745a136e9052efd89b1f4995
Maria108 <maria.abash@gmail.com>
2018-06-03 13:57:08 -0400

    S3

--- src/handlers/generate.js
+++ src/handlers/generate.js
@@ -5,17 +5,17 @@ export const generate = (event, context, callback) => {
     console.log('Stream record: ', JSON.stringify(record, null, 2));
 
     if (record.eventName == 'INSERT') {
-      const id = JSON.stringify(record.dynamodb.NewImage.id.S);
-      const text = JSON.stringify(record.dynamodb.NewImage.text.S);
-      const name = JSON.stringify(record.dynamodb.NewImage.name.S);
-      generateMP3(text, name);
+      const id = record.dynamodb.NewImage.id.S;
+      const text = record.dynamodb.NewImage.text.S;
+      const name = record.dynamodb.NewImage.name.S;
+      generateMP3(id, text, name);
     }
   });
 
   callback(null, `Successfully processed ${event.Records.length} records.`);
 };
 
-function generateMP3(text, name) {
+function generateMP3(id, text, name) {
   const Polly = new AWS.Polly();
 
   const params = {
@@ -26,11 +26,24 @@ function generateMP3(text, name) {
     VoiceId: name,
   };
 
+  console.log(params);
+
   Polly.synthesizeSpeech(params, (err, data) => {
     if (err) {
-      console.error(err.code);
+      console.error('Error: ', err.code);
     } else if (data) {
       if (data.AudioStream instanceof Buffer) {
+        const s3 = new AWS.S3();
+        const s3Params = {
+          Body: data.AudioStream,
+          Bucket: 'voicemail-changer-bucket',
+          Key: `${id}.mp3`,
+        };
+        s3.putObject(s3Params, (err, data) => {
+          if (err) console.log(err, err.stack);
+          // an error occurred
+          else console.log(data); // successful response
+        });
         console.log(JSON.stringify(data.AudioStream));
       }
     }
