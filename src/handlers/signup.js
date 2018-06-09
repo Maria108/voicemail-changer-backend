cc9095ec94d304c873fd89d5ba24e440a89a8e86
Maria108 <maria.abash@gmail.com>
2018-06-08 21:44:57 -0400

    Adds domain name

--- src/handlers/signup.js
+++ src/handlers/signup.js
@@ -6,6 +6,8 @@ export const signup = (event, context, callback) => {
   const requestBody = querystring.parse(event.body);
   const { phone } = requestBody;
 
+  console.log(requestBody);
+
   dbCreateItem(phone);
 
   const response = {
@@ -31,12 +33,39 @@ function dbCreateItem(phone) {
     },
   };
 
+  sendTextMessage(phone, password);
+
   console.log('Adding a new item...');
   ddb.putItem(params, (err, data) => {
     if (err) {
       console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
     } else {
       console.log('Added item:', JSON.stringify(data, null, 2));
+    }
+  });
+}
+
+function sendTextMessage(phone, password) {
+  const params = {
+    Message: `This your password: ${password}` /* required */,
+    MessageAttributes: {
+      'AWS.SNS.SMS.MaxPrice': {
+        DataType: 'Number' /* required */,
+        StringValue: '0.01',
+      },
+      'AWS.SNS.SMS.SMSType': {
+        DataType: 'String' /* required */,
+        StringValue: 'Transactional',
+      },
+    },
+    PhoneNumber: phone,
+  };
+  const sns = new AWS.SNS();
+  sns.publish(params, (err, data) => {
+    if (err) {
+      console.error('Error sending message:', JSON.stringify(err, null, 2));
+    } else {
+      console.log('Message sent:', JSON.stringify(data, null, 2));
     }
   });
 }
